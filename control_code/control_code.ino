a14240974c32bb861125afd402c649913e0c5255
mchouman <mohammedchouman10@gmail.com>
2017-11-21 21:02:49 +0000

    First Draft for Temperature Code

--- control_code/control_code.ino
+++ control_code/control_code.ino
@@ -1,11 +1,44 @@
+#include <math.h>
+
+//there may be some bugs as I haven't tested it yet with actual circuitry
+//the pin numbers and variables will most probably be changed depending on what Anna wants to do with the circuit
+const int thermistorInputPin = 5;
+const int heaterOutputPin = 6; 
+float temperature;
+int T_LB, T_UB;
+float getTemperature(int pinNumber);
+
 void setup()
 {
-  // put your setup code here, to run once:
+  Serial.begin(9600); 
   
+  pinMode(heaterOutputPin, OUTPUT);
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
-  
+  //Temperature code:
+  temperature = getTemperature(thermistorInputPin);
+  if(temperature <= T_LB+2)
+  {
+    digitalWrite(heaterOutputPin, HIGH); //turn heater on
+  }
+  else if(temperature >= T_UB-3)
+  {
+    digitalWrite(heaterOutputPin, LOW); //turn heater off
+  }
+  delay(50);
+}
+
+//Taken from my C Arduino coursework (Mohammed Chouman)
+//Use the Steinhart-Hart equation to convert to degrees C
+float getTemperature(int pinNumber) 
+{
+ int rawADC = analogRead(pinNumber);
+ rawADC -= 200; 
+ float temp;
+ temp = logf(((10240000/rawADC) - 10000));
+ temp = 1 / (0.001129148 +
+ (0.000234125 + (0.0000000876741 * temp * temp ))* temp );
+ return temp - 273.15; // Convert Kelvin to Celsius
 }
